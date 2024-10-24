c7c362b861777d4aa04276f0df0191088b5eed5b
theo.badoz <theo.badoz@gmail.com>
2024-10-24 14:36:40 +0200

    try different scaling x encoding

--- pages/2_modelisation.py
+++ pages/2_modelisation.py
@@ -2,10 +2,30 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from utils.cleaning import load_ks_data, clean_raw_ks_df, feature_eng
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import Normalizer, StandardScaler, RobustScaler
+
 
 st.set_page_config(page_title="Modelisation", page_icon="✍️", layout="wide")
 
+SCALERS = {
+     "normalization": Normalizer(),
+     "standardization": StandardScaler(),
+     "robust": RobustScaler(),
+}
 
+# Define features and target
+features = ['goal', 'timedelta', 'category', 'main_category', 'currency', 'country', 'year', 'month']
+categorical_features = ['category', 'main_category', 'currency', 'country']
+features_to_scale = ['goal', 'timedelta']
+target = 'label'
+
+st.markdown("### Original DF")
+st.dataframe(clean_raw_ks_df(load_ks_data()).head())
 feature_df = (
     load_ks_data()
     .pipe(clean_raw_ks_df)
@@ -14,30 +34,24 @@ feature_df = (
 
 st.dataframe(feature_df.head())
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import StandardScaler
-
-# Define features and target
-features = ['goal', 'timedelta', 'category', 'main_category', 'currency', 'country']
-target = 'label'
-
 # Prepare the data
 X = feature_df[features]
 y = feature_df[target]
 
+
 # Perform one-hot encoding for categorical variables
-X = pd.get_dummies(X, columns=['category', 'main_category', 'currency', 'country'])
+X = pd.get_dummies(X, columns=categorical_features)
 
 # Split the data into train, validation, and test sets
 X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1) # X_test : 10% ; X_train_val: 90%
 X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=.15 * 10/9) # X_val is 15% ; X_train: 75%
 
-# Scale numerical features
-scaler = StandardScaler()
+
+
+st.markdown(f"## chosen scaler: robust") # ["normalization", "standardization", "robust"]:
+
+scaler = SCALERS['robust']
+
 X_train_scaled = scaler.fit_transform(X_train)
 X_val_scaled = scaler.transform(X_val)
 X_test_scaled = scaler.transform(X_test)
@@ -67,12 +81,11 @@ for name, model in models.items():
     st.write("Classification Report:")
     st.text(classification_report(y_val, y_val_pred))
 
-# Feature importance for Random Forest 
+# # Feature importance for Random Forest 
 
-st.subheader("Feature Importance from the Random Forest model")
-feature_importance = pd.DataFrame({
-    'feature': X.columns,
-    'importance': models["Random Forest"].feature_importances_
-}).sort_values('importance', ascending=False)
-st.dataframe(feature_importance)
-
+# st.subheader("Feature Importance from the Random Forest model")
+# feature_importance = pd.DataFrame({
+#     'feature': X.columns,
+#     'importance': models["Random Forest"].feature_importances_
+# }).sort_values('importance', ascending=False)
+# st.dataframe(feature_importance)
--- utils/cleaning.py
+++ utils/cleaning.py
@@ -1,6 +1,13 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OrdinalEncoder, Normalizer, StandardScaler, RobustScaler
+
+
+SCALERS = {
+     "normalization": Normalizer(),
+     "standardization": StandardScaler(),
+     "robust": RobustScaler(),
+}
 
 
 def load_ks_data():
@@ -27,16 +34,48 @@ def clean_raw_ks_df(raw_df):
     return clean_df
 
 
-def feature_eng(clean_df):
-    feature_df = (clean_df
-                  .assign(
-                      # Convert 'launched' and 'deadline' to datetime 
-                      launched=lambda df: pd.to_datetime(df['launched']),
-                      deadline=lambda df: pd.to_datetime(df['deadline']),
-                      # Compute timedelta
-                      timedelta=lambda df: (df['deadline'] - df['launched']).dt.days,
-                      # Define label
-                      label=lambda df: np.where(df['state'] == 'successful', 1, 0)
-                      )                  
-    )
+def encode_categorical_columns(df, categorical_columns):
+    if categorical_columns == "None":
+         return df
+    encoder = OrdinalEncoder()
+    encoded_features = encoder.fit_transform(df[categorical_columns])
+    encoded_feature_names = encoder.get_feature_names_out(categorical_columns)
+    encoded_df = pd.DataFrame(encoded_features, columns=encoded_feature_names, index=df.index)
+    
+    numeric_columns = list(set(df.columns).difference(set(categorical_columns)))
+    encoded_df = pd.concat([df[numeric_columns], encoded_df], axis=1)
+
+    return encoded_df
+
+
+def scaling(df, columns_to_scale, scaler):
+    if scaler == "None":
+         return df
+    not_scaled_columns = list(set(df.columns).difference(set(columns_to_scale)))
+    scaler = SCALERS[scaler]
+    scaled_data = scaler.fit_transform(df[columns_to_scale])
+    scaled_df = pd.DataFrame(scaled_data, columns=columns_to_scale)
+    scaled_df = scaled_df.merge(df[not_scaled_columns], on=df.index, how='left').drop(columns=['key_0'])
+    return scaled_df
+
+
+def feature_eng(clean_df, categorical_columns, columns_to_scale, scaler):
+    feature_df = (
+         clean_df
+         .assign(
+              # Convert 'launched' and 'deadline' to datetime 
+              launched=lambda df: pd.to_datetime(df['launched']),
+              deadline=lambda df: pd.to_datetime(df['deadline']),
+              # Compute timedelta
+              timedelta=lambda df: (df['deadline'] - df['launched']).dt.days,
+              # Extract year and month of campaign launch
+              year=lambda x: x["launched"].dt.year,
+              month=lambda x: x["launched"].dt.month,
+              # Define label
+              label=lambda df: np.where(df['state'] == 'successful', 1, 0)
+         )
+         #.pipe(encode_categorical_columns, categorical_columns)      
+         #.pipe(scaling, columns_to_scale, scaler)
+         
+         )
     return feature_df
