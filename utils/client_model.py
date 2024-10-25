41a301df1317c6d76db055b90b1ea1a9762ad5ff
theo.badoz <theo.badoz@gmail.com>
2024-10-25 11:44:08 +0200

    refacto: prepare every functions required for user usage

--- utils/client_model.py
+++ utils/client_model.py
@@ -1,28 +1,35 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from utils.cleaning import load_ks_data, clean_raw_ks_df, feature_eng
 from xgboost import XGBClassifier
 from sklearn.preprocessing import RobustScaler
 from sklearn.ensemble import RandomForestClassifier
 
 
-# Define features and target
-features = ['goal', 'timedelta', 'category', 'main_category', 'currency', 'country', 'year', 'month']
-categorical_features = ['category', 'main_category', 'currency', 'country']
-target = 'label'
-feature_df = (
-    load_ks_data()
-    .pipe(clean_raw_ks_df)
-    .pipe(feature_eng)
-)
-# Prepare the data
-X = feature_df[features]
-y = feature_df[target]
-# Perform one-hot encoding for categorical variables
-X = pd.get_dummies(X, columns=categorical_features)
-# Scaling
-scaler = RobustScaler()
-X_train_scaled = scaler.fit_transform(X)
+def init_client_model():
+    # Define features and target
+    features = ['goal', 'timedelta', 'category', 'main_category', 'currency', 'country', 'year', 'month']
+    categorical_features = ['category', 'main_category', 'currency', 'country']
+    target = 'label'
+    feature_df = (
+        load_ks_data()
+        .pipe(clean_raw_ks_df)
+        .pipe(feature_eng)
+    )
+    # Prepare the data
+    X = feature_df[features]
+    y = feature_df[target]
+    # Perform one-hot encoding for categorical variables
+    X = pd.get_dummies(X, columns=categorical_features)
+    # Scaling
+    scaler = RobustScaler()
+    
+    st.session_state["X"] = X
+    st.session_state["y"] = y
+    st.session_state["scaler"] = scaler.fit(X)
+    st.session_state["X_scaled"] = st.session_state["scaler"].transform(X)
 
 
 def train_client_model():
@@ -30,22 +37,24 @@ def train_client_model():
     model = XGBClassifier(random_state=42)
 
     # Train the model
-    model.fit(X_train_scaled, y)
-    return model
-
-
-    # # Make predictions on validation set
-    # y_val_pred = model.predict(X_val_scaled)
+    model.fit(st.session_state["X_scaled"], st.session_state["y"])
+    st.session_state["model"] = model
 
 
 def get_feature_importance():
     # Feature importance for Random Forest 
     feature_importance_model = RandomForestClassifier(random_state=42)
-    feature_importance_model.fit(X, y)
+    feature_importance_model.fit(st.session_state["X"], st.session_state["y"])
     st.subheader("Feature Importance from the Random Forest model")
     feature_importance = pd.DataFrame({
-        'feature': X.columns,
+        'feature': st.session_state["X"].columns,
         'importance': feature_importance_model.feature_importances_
     }).sort_values('importance', ascending=False)
-    return feature_importance
+    st.session_state["feature_importance"] = feature_importance
 
+
+def plot_feature_importance():
+        fig_importance = plt.figure(figsize=(10, 6))
+        sns.barplot(x='importance', y='feature', data=st.session_state["feature_importance"])
+        plt.title("Feature Importance")
+        st.pyplot(fig_importance)
